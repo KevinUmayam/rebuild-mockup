19e8819d0b97d209a9b5133ff4341797ffdb8a3e
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-06-09 11:20:45 -0700

    finished recibir pedido

--- client/src/scenes/recibirPedido/index.jsx
+++ client/src/scenes/recibirPedido/index.jsx
@@ -1,21 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
+import { BsFillTriangleFill} from 'react-icons/bs'
+import { CgShapeRhombus } from 'react-icons/cg'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const RecibirPedido = () => {
-   const [choices, setChoices] = useState([
-    'Option 1',
-    'Option 2'
+  // opciones para el dropdown menu
+  const [choices, setChoices] = useState([
+    '001-Ashlee',
+    '002-Carlos',
+    '003-Kevin',
+    '004-Michell',
   ]);
+  // usestate para busqueda y visibilidad de menu
   const [searchQuery, setSearchQuery] = useState('');
   const [showDropdown, setShowDropdown] = useState(false);
 
+  // leer el valor de la busqueda
   const handleInputChange = (event) => {
     setSearchQuery(event.target.value);
   };
-
+  // toggle para el menu
   const handleDropdownToggle = () => {
     setShowDropdown(!showDropdown);
   };
-
+ // agregar persona a el menu de busqueda 
   const handleAddChoice = () => {
     if (searchQuery.trim() !== '') {
       setChoices([...choices, searchQuery]);
@@ -23,67 +31,134 @@ const RecibirPedido = () => {
       setShowDropdown(false);
     }
   };
-
+  // filtrar para que las busqueda sea mas facil 
   const filteredChoices = choices.filter(choice =>
     choice.toLowerCase().includes(searchQuery.toLowerCase())
   );
-
   return (
-    <div className="bg-custom-light-gray w-screen h-screen flex justify-end">
-      <div className="bg-custom-gray h-screen w-1180 ">
+    // gray page div
+<div className="bg-custom-light-gray w-screen h-screen flex justify-end">
+  <div className="bg-custom-gray h-screen w-1180 ">
 
-        <div className="flex justify-between">
-          <div className="bg-custom-green w-240 h-60  rounded-br-lg border-2 border-black text-center"><h1 className="color text-white text-30">DD/MM/AAA</h1></div>
-          <div className="bg-custom-green w-240 h-60 rounded-bl-lg border-2 border-black text-center"><h1 className="color text-white text-30">folio:000001</h1></div>
-        </div>
+{/* Divs de arriba */}
+    <div className="flex justify-between">
+      <div className="bg-custom-green w-240 h-50  rounded-br-lg border-2 border-black text-center">
+        <h1 className="color text-white text-30">DD/MM/AAA</h1>
+      </div>
+      <div className="bg-custom-green w-240 h-50 rounded-bl-lg border-2 border-black text-center">
+        <h1 className="color text-white text-30">folio:000001</h1>
+      </div>
+    </div>
 
-        <div className="w-480 h-60 flex">
-        <div>Cliente:</div>
-        <div>  
-           <div className="relative">
-      <div className="relative inline-block w-64">
-        <input
+{/* Divs de busqueda */}
+    <div className="w-480 h-50 mt-10 mb-10 ml-10px flex">
+      <div className='text-white text-30'>Cliente:</div>
+      {/*  input funcional  */}
+        <div className='flex flex-row'> 
+            <div className="relative">
+              <div className="relative flex flex-row">
+                <input
           type="text"
           value={searchQuery}
           onChange={handleInputChange}
-          className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none"
-          placeholder="Search..."
-        />
-        <button
+          className="w-240 h-50 rounded-l-md border-2 border-black focus:outline-none bg-custom-light-gray text-30"
+                />
+                < button
           onClick={handleDropdownToggle}
-          className="absolute right-0 top-0 h-full px-4 bg-green-500 text-white font-semibold rounded-md"
-        >
-          {showDropdown ? 'Close' : 'Open'}
-        </button>
-      </div>
-
-      {showDropdown && (
-        <ul className="mt-2 p-2 bg-white border border-gray-300 rounded-md shadow-md">
-          {filteredChoices.map((choice, index) => (
-            <li key={index} className="px-4 py-2 cursor-pointer hover:bg-gray-100">
-              {choice}
-            </li>
-          ))}
-          {filteredChoices.length === 0 && (
-            <li className="px-4 py-2 text-gray-500">No matching options</li>
-          )}
-          <li className="px-4 py-2">
-            <button
+          className=" right-0 top-0 h-50 w-60 bg-custom-green text-white border-2 border-black flex justify-center items-center"
+                >
+          {/* {showDropdown ? 'Close' : 'Open'} */}
+          {showDropdown ?  <BsFillTriangleFill size={30} /> : <BsFillTriangleFill className='transform rotate-180' size={30}/>}
+              </button>
+              <button
               onClick={handleAddChoice}
-              className="text-green-500 hover:text-green-700"
-            >
-              Add "{searchQuery}"
-            </button>
-          </li>
-        </ul>
-      )}
-    </div>
-    </div>
-    </div>
-        <div></div>
-      </div>
-      </div>
+                className=" right-0 top-0 h-50 w-60 bg-custom-green text-white rounded-r-md border-2 border-black flex justify-center items-center"
+              >
+              <CgShapeRhombus size={35} />
+              </button>
+              </div>
+              {showDropdown && (
+                <ul className="w-240 bg-custom-light-gray border border-black shadow-md">
+                  {filteredChoices.map((choice, index) => (
+                  <li key={index} className="px-4 py-2 cursor-pointer hover:bg-gray-100">
+                  {choice}
+                  </li>
+                    ))}
+                  </ul>
+                      )}
+                    </div> 
+                {filteredChoices.length === 0 && (
+            <div className='w-240 flex justify-center items-center'><h1 className=" font-medium text-red-600">Usuario No Encontrado</h1></div>
+          )}
+          </div>
+           {/* fin de input funcional  */}
+     </div>
 
+{/* div de cuadro grid con borders */}
+<div className='flex justify-center items-center flex-col'>
+    <div className='h-16 w-1160 bg-custom-gray border-2 border-b-0 border-black flex items-center justify-evenly'>
+      <h1 className='text-white text-30 ml-5'>Prenda:</h1>
+          {/* input de prenda */}
+        <div className='flex flex-row'> 
+          <input
+          type="text"
+          className="w-220 h-50 rounded-l-md border-2 border-black focus:outline-none bg-custom-light-gray text-30"
+          />
+          <button
+          className=" right-0 top-0 h-50 w-60 bg-custom-green text-white border-2 border-black flex justify-center items-center rounded-r-md"
+          >
+          <BsFillTriangleFill className='transform rotate-180' size={30}/>
+          </button>
+      </div> 
+        {/* input de cantidad */}
+      <h1 className='text-black text-30 '>Cantidad:</h1>
+       
+        <input
+          type="text"
+          className="w-120 h-50 rounded-md border-2 border-black focus:outline-none bg-custom-light-gray text-30"
+        />
+         <button
+          className=" right-0 top-0 h-50 w-28 bg-custom-green text-white border-2 border-black flex justify-center items-center rounded-md"
+        >
+          <h1 className='text-white text-30 '>OK</h1>
+        </button>
+        <h1 className='text-white text-30 mr-5'>Total: $000.00</h1>
+    </div> 
+<div className='h-460 w-1160 border-t-2 border-b-2  border-black grid grid-cols-recibir'>
+      <div className="bg-custom-light-gray border border-l-2 border-black">
+        <div className=' bg-custom-gray h-60 w-270 flex justify-center items-center border-b-2 border-black'>
+          <h1 className='text-white text-25'>Camisa-001</h1>
+          </div>
+      </div>
+      <div className="bg-custom-light-gray border border-black">
+        <div className=' bg-custom-gray h-60 w-270 flex justify-center items-center border-b-2 border-black'>
+          <h1 className='text-white text-25'>2 Unids</h1>
+          </div>
+      </div>
+      <div className="bg-custom-light-gray border border-black">
+        <div className=' bg-custom-gray h-60 w-270 flex justify-center items-center border-b-2 border-black'>
+          <h1 className='text-white text-25'>$100.00</h1>
+          </div>
+      </div>
+      <div className="bg-custom-light-gray border border-black">
+        <div className=' bg-custom-gray h-60 w-270 flex justify-center items-center border-b-2 border-black'>
+          <h1 className='text-white text-25'>$200.00</h1>
+          </div>
+      </div>
+      <div className="bg-custom-light-gray border border-r-2 border-black">
+        <div className=' bg-custom-gray h-60 w-78 flex justify-center items-center border-b-2 border-black'>
+          <AiOutlineClose className=" text-red-500" size={55}></AiOutlineClose>
+          </div>
+      </div>
+      </div>
+</div>
+{/* botton al final de la pagina */}
+<div className="flex justify-end mr-10px items-center">
+          <button className="bg-custom-green w-240 h-50 mt-5 rounded-lg border-2 border-black text-center"><h1 className="color text-white text-30">Confirmar Pedido</h1></button>
+        </div>
+      </div>
+      
+    </div>
   );
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,18 +14,32 @@ module.exports = {
       },
       spacing: {
         '1180': '1180px',
+        '1160': '1160px',
         '1024': '1024px',
+        '600': '600px',
+        '560': '560px',
         '480': '480px',
+        '460': '460px',
+        '260': '260px',
         '250': '250px',
         '240': '240px',
         '230': '230px',
+        '220': '220px',
+        '120': '120px',
+        '80': '80px',
         '60': '60px',
         '50': '50px',
         '30': '30px',
         '10px': '10px',
       },
       fontSize: {
-        '30': '30px', // Custom font size
+        '30': '30px', // Custom font 
+        '25': '25px', // Custom font size
+      },
+      gridTemplateColumns: {
+
+        // Complex site-specific column configuration
+        'recibir': 'repeat(4, 270px) 80px',
       },
     },
   },
